9cd549efda1e6f8583e97f8c05060d933ffdd1d2
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-16 12:30:24 +0100

    Solucionado

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 // Tu código aquí
+const form = document.forms["message-form"];
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-form.addEventListener('submit', function(e){
-    console.log("Hemos hecho clic en el botón de submit!")
+    const inputMessage = document.querySelector("#message");
+    const messageDelivered = document.querySelector(".message-content");
+    const errorMsg = document.querySelector(".feedback");
+
+    if (inputMessage.value.trim() == "") {
+        errorMsg.classList.add("show");
+        inputMessage.value = "";
+        return;
+    } else {
+        errorMsg.classList.remove("show");
+    }
+
+    messageDelivered.textContent = inputMessage.value.trim();
+    inputMessage.value = "";
 
 })
