68b3a36598f5e66a8f7f8c6dbc690fa0d98871e4
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-16 13:08:26 +0100

    2.0

--- js/app.js
+++ js/app.js
@@ -1,22 +1,24 @@
-// Tu código aquí
 const form = document.forms["message-form"];
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const inputMessage = document.querySelector("#message");
+    const inputMessage = e.target.elements["message"];
+
     const messageDelivered = document.querySelector(".message-content");
     const errorMsg = document.querySelector(".feedback");
 
     if (inputMessage.value.trim() == "") {
         errorMsg.classList.add("show");
+
+        setTimeout(() => {
+            errorMsg.classList.remove("show");
+        }, 1500);
+
         inputMessage.value = "";
         return;
-    } else {
-        errorMsg.classList.remove("show");
     }
 
     messageDelivered.textContent = inputMessage.value.trim();
     inputMessage.value = "";
-
 })
